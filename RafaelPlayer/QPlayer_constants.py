1925c93acb48feb7303c7fd4d84f7c6d426bb7ec
InbalWaiss <76895642+InbalWaiss@users.noreply.github.com>
2021-02-21 15:14:28 +0200

    packages

--- RafaelPlayer/QPlayer_constants.py
+++
@@ -1,8 +0,0 @@
-epsilon = 0.5 #randomness
-START_EPSILON = epsilon #for statistics
-EPSILONE_DECAY = 0.99999 #every episode will be epsilon*EPISODE_DECAY
-min_epsilon = 0.05
-LEARNING_RATE = 0.5
-DISCOUNT = 0.95
-
-
